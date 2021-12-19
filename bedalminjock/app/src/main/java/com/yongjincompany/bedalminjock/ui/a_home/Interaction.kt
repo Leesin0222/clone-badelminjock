000a3a115743674338fc190cd59b691186f0b537
Leesin0222 <dragon02229@naver.com>
2021-12-20 08:57:13 +0900

    📑 :: Interaction 이미지배너 클릭 이벤트 추가

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/Interaction.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/Interaction.kt
@@ -1,4 +1,8 @@
 package com.yongjincompany.bedalminjock.ui.a_home
 
+import com.yongjincompany.bedalminjock.model.BannerItem
+
 interface Interaction {
+    //해당 인터페이스로 이미지 배너를 클릭시 동작이 되게...
+    fun onBannerItemClicked(bannerItem: BannerItem)
 }
