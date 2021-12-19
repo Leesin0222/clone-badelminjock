3e6d78344bd7ef46ddc8b50791a06a110ea11967
Leesin0222 <dragon02229@naver.com>
2021-12-20 08:53:32 +0900

    📑:: 뷰모델 수정

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/MainActivityViewModel.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/MainActivityViewModel.kt
@@ -7,12 +7,26 @@ import com.yongjincompany.bedalminjock.model.BannerItem
 
 class MainActivityViewModel : ViewModel() {
     private val _bannerItemList: MutableLiveData<List<BannerItem>> = MutableLiveData()
+    private val _currentPosition: MutableLiveData<Int> = MutableLiveData()
 
     val bannerItemList : LiveData<List<BannerItem>>
         get() = _bannerItemList
+
+    val currentPosition: LiveData<Int>
+        get() = _currentPosition
+
+    init{
+        _currentPosition.value=0
+    }
 
 
     fun setBannerItem(list: List<BannerItem>){
         _bannerItemList.value = list
     }
+
+    fun setCurrentPosition(position: Int) {
+        _currentPosition.value = position
+    }
+
+    fun getcurrentPosition() = currentPosition.value
 }
