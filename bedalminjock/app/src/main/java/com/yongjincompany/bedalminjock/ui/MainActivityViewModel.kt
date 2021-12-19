7fcecb07b3f432130f9af55bcfaaa204e0274065
Leesin0222 <dragon02229@naver.com>
2021-12-19 19:40:38 +0900

    📑 :: 메인엑티비티 ViewModel 만듬

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/MainActivityViewModel.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/MainActivityViewModel.kt
@@ -1,2 +1,18 @@
 package com.yongjincompany.bedalminjock.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.yongjincompany.bedalminjock.model.BannerItem
+
+class MainActivityViewModel : ViewModel() {
+    private val _bannerItemList: MutableLiveData<List<BannerItem>> = MutableLiveData()
+
+    val bannerItemList : LiveData<List<BannerItem>>
+        get() = _bannerItemList
+
+
+    fun setBannerItem(list: List<BannerItem>){
+        _bannerItemList.value = list
+    }
+}
