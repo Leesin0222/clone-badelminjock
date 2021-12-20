7cffc50659d2cb2add3c6eda3fcd79c6b0320bec
Leesin0222 <dragon02229@naver.com>
2021-12-20 12:00:48 +0900

    📑 :: Grid item 관련 LiveData 생성

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/MainActivityViewModel.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/MainActivityViewModel.kt
@@ -4,16 +4,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.yongjincompany.bedalminjock.model.BannerItem
+import com.yongjincompany.bedalminjock.model.GridItem
 
 class MainActivityViewModel : ViewModel() {
+
     private val _bannerItemList: MutableLiveData<List<BannerItem>> = MutableLiveData()
     private val _currentPosition: MutableLiveData<Int> = MutableLiveData()
+    private val _gridItemList: MutableLiveData<List<GridItem>> = MutableLiveData()
 
     val bannerItemList : LiveData<List<BannerItem>>
         get() = _bannerItemList
 
     val currentPosition: LiveData<Int>
         get() = _currentPosition
+
+    val gridItemList: LiveData<List<GridItem>>
+        get() = _gridItemList
 
     init{
         _currentPosition.value=0
@@ -26,6 +32,10 @@ class MainActivityViewModel : ViewModel() {
 
     fun setCurrentPosition(position: Int) {
         _currentPosition.value = position
+    }
+
+    fun setGridItems(list: List<GridItem>) {
+        _gridItemList.value = list
     }
 
     fun getcurrentPosition() = currentPosition.value
