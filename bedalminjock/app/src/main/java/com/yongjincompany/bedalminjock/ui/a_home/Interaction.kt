981973a117a17a01fe4d40df5b461b2f23b3563a
Leesin0222 <dragon02229@naver.com>
2021-12-20 09:56:33 +0900

    📑 :: onClickListener를 빼먹었잫아..

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/Interaction.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/Interaction.kt
@@ -1,8 +1,9 @@
 package com.yongjincompany.bedalminjock.ui.a_home
 
+import android.view.View
 import com.yongjincompany.bedalminjock.model.BannerItem
 
-interface Interaction {
+interface Interaction : View.OnClickListener {
     //해당 인터페이스로 이미지 배너를 클릭시 동작이 되게...
     fun onBannerItemClicked(bannerItem: BannerItem)
 }
