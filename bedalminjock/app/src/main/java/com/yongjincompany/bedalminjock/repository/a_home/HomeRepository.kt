b722eddeca8f2c4f1697e35eac54cd33d6f1126c
Leesin0222 <dragon02229@naver.com>
2021-12-21 11:47:13 +0900

    📑 :: suspend 함수로 getBannerItems와 getGridItems 선언

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/repository/a_home/HomeRepository.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/repository/a_home/HomeRepository.kt
@@ -1,4 +1,9 @@
 package com.yongjincompany.bedalminjock.repository.a_home
 
+import com.yongjincompany.bedalminjock.model.BannerItem
+import com.yongjincompany.bedalminjock.model.GridItem
+
 interface HomeRepository {
+    suspend fun getBannerItems() : List<BannerItem>
+    suspend fun getGridItems() : List<GridItem>
 }
