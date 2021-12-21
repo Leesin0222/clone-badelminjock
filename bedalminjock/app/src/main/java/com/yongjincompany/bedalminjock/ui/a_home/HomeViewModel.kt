f2a58d7437ea06ba949f2dd80615237ae8c86a16
Leesin0222 <dragon02229@naver.com>
2021-12-21 11:17:53 +0900

    📑 :: Home뷰모델

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/HomeViewModel.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/HomeViewModel.kt
@@ -1,4 +1,50 @@
 package com.yongjincompany.bedalminjock.ui.a_home
 
-class HomeViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yongjincompany.bedalminjock.model.BannerItem
+import com.yongjincompany.bedalminjock.model.GridItem
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class HomeViewModel : ViewModel() {
+
+    private val _bannerItemList: MutableLiveData<List<BannerItem>> = MutableLiveData()
+    private val _gridItemList : MutableLiveData<List<GridItem>> = MutableLiveData()
+    private val _currentPosition : MutableLiveData<Int> = MutableLiveData()
+
+    val bannerItemList : LiveData<List<BannerItem>>
+        get() = _bannerItemList
+    val gridItemList : LiveData<List<GridItem>>
+        get() = _gridItemList
+    val currentPosition : LiveData<Int>
+        get() = _currentPosition
+
+    init {
+        _currentPosition.value = 0
+    }
+
+    fun setCurrentPosition(position : Int) {
+        _currentPosition.value = position
+    }
+    fun getBannerItems() {
+        viewModelScope.launch {
+            val bannerItemLiveData = HomeRepositoryImpl.getGridItems()
+            withContext(Main) {
+                _bannerItemList.value = bannerItemLiveData
+            }
+        }
+    }
+
+    fun getGridItems() {
+        viewModelScope.launch {
+            val gridItemLiveData = HomeRepositoryImpl.getGridItems()
+            withContext(Main) {
+                _gridItemList.value = gridItemLiveData
+            }
+        }
+    }
 }
