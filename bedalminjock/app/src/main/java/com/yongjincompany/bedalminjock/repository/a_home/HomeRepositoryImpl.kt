6160ab794f7d0417f15015d98a1b74f50b490e43
Leesin0222 <dragon02229@naver.com>
2021-12-21 11:51:27 +0900

    📑 :: 싱글톤

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/repository/a_home/HomeRepositoryImpl.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/repository/a_home/HomeRepositoryImpl.kt
@@ -1,4 +1,18 @@
 package com.yongjincompany.bedalminjock.repository.a_home
 
-object HomeRepositorylmpl {
+import com.yongjincompany.bedalminjock.model.BannerItem
+import com.yongjincompany.bedalminjock.model.GridItem
+import com.yongjincompany.bedalminjock.model.data.fakeBannerItemList
+import com.yongjincompany.bedalminjock.model.data.fakeGridItemList
+
+//싱글(벙글)톤
+object HomeRepositoryImpl : HomeRepository {
+    
+    override suspend fun getBannerItems(): List<BannerItem> {
+        return fakeBannerItemList
+    }
+
+    override suspend fun getGridItems() : List<GridItem> {
+        return fakeGridItemList
+    }
 }
