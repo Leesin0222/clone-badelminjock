5ff577917fad39d786c3d28490efe27483cd50f4
Leesin0222 <dragon02229@naver.com>
2021-12-21 12:03:47 +0900

    📑 :: 이걸 깜빡하다니...

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/HomeFragment.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/ui/a_home/HomeFragment.kt
@@ -2,10 +2,8 @@ package com.yongjincompany.bedalminjock.ui.a_home
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
@@ -33,6 +31,9 @@ class HomeFragment : Fragment(R.layout.fragment_home), Interaction {
         iv_arrow.setOnClickListener(this)
 
         initViewPager2()
+        initGridRecyclerView()
+        autoScrollViewPager()
+        subscribeObservers()
     }
 
     private fun subscribeObservers() {
