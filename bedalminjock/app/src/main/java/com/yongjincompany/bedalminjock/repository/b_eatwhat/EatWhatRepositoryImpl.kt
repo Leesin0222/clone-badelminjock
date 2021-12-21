5255d990463708a8af33da6ba43b9f5a1b1f850a
Leesin0222 <dragon02229@naver.com>
2021-12-21 14:14:24 +0900

    📑 :: getWhatToEatItems 에 리턴 fakeWhatToEatList

--- bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/repository/b_eatwhat/EatWhatRepositoryImpl.kt
+++ bedalminjock/app/src/main/java/com/yongjincompany/bedalminjock/repository/b_eatwhat/EatWhatRepositoryImpl.kt
@@ -1,4 +1,10 @@
 package com.yongjincompany.bedalminjock.repository.b_eatwhat
 
+import com.yongjincompany.bedalminjock.model.WhatToEat
+import com.yongjincompany.bedalminjock.model.data.fakeWhatToEatList
+
 object EatWhatRepositoryImpl {
+    override suspend fun getWhatToEatItems() : List<WhatToEat> {
+        return fakeWhatToEatList
+    }
 }
